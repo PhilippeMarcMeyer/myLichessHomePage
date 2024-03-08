80489b7c48d89dcf3e9f6b73864d96fd30d35270
philippe MEYER <pmg.meyer@gmail.com>
2024-03-08 04:22:50 +0100

    changed style

--- dontwanna.js
+++ dontwanna.js
@@ -39,6 +39,7 @@ const runOnce = () => {
         target.forEach((x) => {
             x.style.border = '1px solid rgba(255,255,255,0.5)';
             x.borderRadius = '10px';
+            x.style.background = 'transparent'
         });
     }
 
@@ -46,7 +47,39 @@ const runOnce = () => {
     target = document.querySelector('#main-wrap');
     if (target) {
         target.style.minHeight = '1200px';
+
     }
+
+    target = document.querySelector('body');
+    if (target) {
+        target.style.background = '#142233';
+    }
+
+    target = document.querySelector('.enterable_list.lobby__box__content');
+    if (target) {
+        target.style.background = 'transparent';
+    }
+
+    target = document.querySelector('.lobby__tournaments.lobby__box > a');
+    if (target) {
+        target.style.background = 'rgba(255,255,255,0.1)';
+    }
+
+    target = document.querySelectorAll('.lobby__app__content.lpools > div');
+    if (target) {
+        target.forEach((x) => {
+            x.style.border = '1px solid rgba(255,255,255,0.5)';
+        });
+    }
+
+    target = document.querySelectorAll('.lobby__box tr:nth-child(even)');
+    if (target) {
+        target.forEach((x) => {
+            x.style.background = 'rgba(255,255,255,0.1)';
+        });
+    }
+
+ 
 
     run(); // Will also run once at startup and then when the MutationObserver reports changes in the zone
 };
@@ -56,32 +89,44 @@ const run = () => {
     isRunning = true;
     let target = document.querySelector('.lobby__app__content.lpools');
     if (target) {
-        target.style.gridTemplateRows = 'none';
+        if(target.style.gridTemplateRows !== 'none') target.style.gridTemplateRows = 'none';
+        if(target.style.background !== 'transparent') target.style.background = 'transparent';
         ['1+0', '2+1', '3+0', '3+2'].forEach((x) => {
             let subTarget = target.querySelector(`[data-id='${x}']`);
             if (subTarget) {
-                subTarget.style.display = 'none';
+                if(subTarget.style.display !== 'none') subTarget.style.display = 'none';
             }
         });
     }
-    target = document.querySelectorAll('.lobby__app__content.lpools > div');
+    target = document.querySelector('.lobby__app__content.lreal_time');
+    if (target) {
+       if(target.style.background !== 'transparent') target.style.background = 'transparent';
+    }
+    target = document.querySelector('.lobby__app__content.lseeks');
+    if (target) {
+        if(target.style.background !== 'transparent')  target.style.background = 'transparent';
+    }    
+    target = document.querySelector('.lobby__app__content.lnow_playing');
+    if (target) {
+        if(target.style.background !== 'transparent')  target.style.background = 'transparent';
+    }
+    target = document.querySelectorAll('.lobby__spotlights > a');
     if (target) {
         target.forEach((x) => {
-            x.style.border = '1px solid rgba(255,255,255,0.5)';
+            if(x.style.background !== 'transparent')   x.style.background = 'transparent';
         });
     }
+    
     isRunning = false;
 };
 
 const launchObserver = () => {
-        // check if the the lobby zone encounters changes to re-run run()
-    let target = document.querySelector('.lobby__app__content.lpools');
-    if (target) {
-        const observer = new MutationObserver((mutations) => {
-            if (!isRunning) run();
-        });
-        observer.observe(document, { childList: true, subtree: true });
-    }
+    const observer = new MutationObserver((mutations) => {
+        let target = document.querySelector('.lobby__app');
+
+        if (!isRunning && target) run();
+    });
+    observer.observe(document, { childList: true, subtree: true });
 };
 
 const init = () => {
