1d1bef3784aa9a21cc1dd04ed1ea4ed1f8a46606
philippe MEYER <pmg.meyer@gmail.com>
2024-05-26 19:12:44 +0200

    alternate log

--- dontwanna.js
+++ dontwanna.js
@@ -610,15 +610,23 @@ const runOnce = (targets, challenges) => {
             target.style.display = 'none';
         }
     });
-
-    const logo = `<img id='logoLichess' src="data:image/svg+xml,%3Csvg viewBox='-2 -2 54 54' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='${colors.logo}' stroke='${colors.logo}' stroke-linejoin='round'%0Ad='M38.956.5c-3.53.418-6.452.902-9.286 2.984C5.534 1.786-.692 18.533.68 29.364 3.493 50.214 31.918 55.785 41.329 41.7c-7.444 7.696-19.276 8.752-28.323 3.084C3.959 39.116-.506 27.392 4.683 17.567 9.873 7.742 18.996 4.535 29.03 6.405c2.43-1.418 5.225-3.22 7.655-3.187l-1.694 4.86 12.752 21.37c-.439 5.654-5.459 6.112-5.459 6.112-.574-1.47-1.634-2.942-4.842-6.036-3.207-3.094-17.465-10.177-15.788-16.207-2.001 6.967 10.311 14.152 14.04 17.663 3.73 3.51 5.426 6.04 5.795 6.756 0 0 9.392-2.504 7.838-8.927L37.4 7.171z'/%3E%3C/svg%3E%0A"></img>`;
+    let isAlternateLogo = false;
+    let logo = `<img id='logoLichess' src="data:image/svg+xml,%3Csvg viewBox='-2 -2 54 54' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='${colors.logo}' stroke='${colors.logo}' stroke-linejoin='round'%0Ad='M38.956.5c-3.53.418-6.452.902-9.286 2.984C5.534 1.786-.692 18.533.68 29.364 3.493 50.214 31.918 55.785 41.329 41.7c-7.444 7.696-19.276 8.752-28.323 3.084C3.959 39.116-.506 27.392 4.683 17.567 9.873 7.742 18.996 4.535 29.03 6.405c2.43-1.418 5.225-3.22 7.655-3.187l-1.694 4.86 12.752 21.37c-.439 5.654-5.459 6.112-5.459 6.112-.574-1.47-1.634-2.942-4.842-6.036-3.207-3.094-17.465-10.177-15.788-16.207-2.001 6.967 10.311 14.152 14.04 17.663 3.73 3.51 5.426 6.04 5.795 6.756 0 0 9.392-2.504 7.838-8.927L37.4 7.171z'/%3E%3C/svg%3E%0A"></img>`;
+    if(friendsPhotos && friendsPhotos['logo']){
+        isAlternateLogo = true;
+        logo = `<img id='logoLichess' style='margin-top:2%;width:70%;margin-left:15%;border-radius:10px;' src='${friendsPhotos['logo']}'></img>`;
+    }
 
     if (targets.includes('lobby__timeline')) {
         let lobbyPointer = document.querySelector('.lobby__timeline');
         if (lobbyPointer) {
-            lobbyPointer.style.display = 'flex';
-            lobbyPointer.style.borderRadius = '50%';
-            lobbyPointer.innerHTML = logo;
+            if(!isAlternateLogo){
+                lobbyPointer.style.display = 'flex';
+                lobbyPointer.style.borderRadius = '50%';
+            }else{
+                lobbyPointer.style.display = 'block';
+            }
+            lobbyPointer.innerHTML = logo; 
         }
     }
 
